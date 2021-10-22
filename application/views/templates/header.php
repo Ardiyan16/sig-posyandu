77bb98ed5ce1112e797ff7b5d11ea6e16d455029
ajipratama11 <47959982+ajipratama11@users.noreply.github.com>
2021-10-22 10:11:18 +0700

    x
    x

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -10,7 +10,7 @@
     <meta name="author" content="">
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url() ?>assets/images/logo_sd.png">
-    <title>SDN BADEAN 1 BONDOWOSO</title>
+    <title>SIG - POSYANDU</title>
     <!-- Custom CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
 
@@ -22,7 +22,6 @@
     <!-- Page level plugin JavaScript-->
     <script src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js"></script>
     <script src="<?= base_url() ?>vendor/ckeditor/ckeditor.js"></script>
-
     <script src="https://cdn.datatables.net/1.10.18/js/dataTables.bootstrap4.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/extra-libs/multicheck/multicheck.css">
